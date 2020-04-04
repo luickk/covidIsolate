c8dbf944c079015462112384674f4e9b4e1e67ef
luick klippel <gelberdev@gmail.com>
2020-04-04 14:09:42 +0200

    Initial Commit

---
+++ covid-isolate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  covid-isolate
+//
+//  Created by luick klippel on 04.04.20.
+//  Copyright © 2020 luick klippel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
